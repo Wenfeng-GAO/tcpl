6e88bf19156e559e2dd2ea2c2590ffc22a5068ac
wenfeng <elricfeng@gmail.com>
2015-06-16 19:01:57 +0200

    chapter 4

--- chapter4/exc4_10.c
+++ chapter4/exc4_10.c
@@ -6,101 +6,146 @@
  */
 
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 #include <stdlib.h>
 
-#define MAXOP 100 /* Max size of operand or operator */
-#define NUMBER '0' /* Signal that a number was found */
+#define NUMBER '0' /* number signal */
+#define MAXLINE 100 /* max length of an input line */
+#define MAXVAL 100 /* max length of val array */
+#define MAXSTACK 100 /* depth of stack */
 
-void push(double);
-double pop(void);
+char line[MAXLINE]; /* input line */
+int lp; /* next unscanned position */
+double stk[MAXSTACK]; /* stack to store pushed numbers */
+int sp; /* next free stack position */
 
-#define MAXLINE 100 /* Maximum input line length */
 int get_line(char line[], int lim);
-char line[MAXLINE];
+void noblanks(void);
+void append(char s[], int c);
+void storenumber(char v[]);
+int getop(char val[]);
+double pop(void);
+void push(double v);
 
-/* reverse Polish calculator */
 int main()
 {
-    /*
-    int type;
-    double op2;
-    char s[MAXOP];
+    char val[MAXVAL];
+    int c;
+    double op1, op2;
 
-    while ((type = getop(s)) != EOF) {
-        switch (type) {
-            case NUMBER:
-                push(atof(s));
-                break;
-            case '+':
-                push(pop() + pop());
-                break;
-            case '*':
-                push(pop() * pop());
-                break;
-            case '/':
-                op2 = pop();
-                if (op2 != 0.0)
-                    push(pop() / op2);
-                else
-                    printf("error: zero divisor\n");
-                break;
-            case '-':
-                op2 = pop();
-                push(pop() - op2);
-                break;
-            case '\n':
-                printf("\t%.8g\n", pop());
-                break;
-            default:
-                printf("error: unknown command %s\n", s); 
-                break;
+    while (get_line(line, MAXLINE)) {
+        lp = 0;
+        while (lp < strlen(line)) {
+            val[0] = '\0';
+            switch (getop(val)) {
+                case NUMBER:
+                    push(atof(val));
+                    break;
+                case '+':
+                    push(pop() + pop());
+                    break;
+                case '*':
+                    push(pop() * pop());
+                    break;
+                case '-':
+                    op2 = pop();
+                    op1 = pop();
+                    push(op1 - op2);
+                    break;
+                case '/':
+                    op2 = pop();
+                    op1 = pop();
+                    if (op2 != 0.0)
+                        push(op1 / op2);
+                    else
+                        printf(" zero denominator\n");
+                    break;
+                case '\n':
+                    printf("\t=%.6g\n", pop());
+                    break;
+            }
         }
     }
-    */
-    while (get_line(line, MAXLINE))
-        printf("%s", line);
 
     return 0;
-
 }
 
-
-
-#define MAXVAL 100 /* Maximum depth of val stack */
-
-int sp = 0; // Next free stack position
-double val[MAXVAL]; // Value stack
-
-/* push: push f onto value stack */
-void push(double f)
-{
-    if (sp < MAXVAL)
-        val[sp++] = f;
-    else
-        printf("error: stack full, can't push %g\n", f);
-}
-
-/* pop: pop and return top value from stack */
+/* pop: pop and return value from top of stack */
 double pop(void)
 {
     if (sp > 0)
-        return val[--sp];
-    else {
-        printf("error: stack empty\n");
-        return 0.0;
+        return stk[--sp];
+    else
+        printf("error: stack is empty, can't pop\n");
+    return 0.0;
+}
+
+/* push: push value on top of stack */
+void push(double v)
+{
+    if (sp < MAXSTACK)
+        stk[sp++] = v;
+    else
+        printf("error: stack is full, can't push\n");
+}
+
+/* noblanks: skip white blanks */
+void noblanks(void)
+{
+    while (line[lp] == ' ' || line[lp] == '\t')
+        ++lp;
+}
+
+/* getop: return operator (or other non number) character */
+int getop(char val[])
+{
+    int c;
+
+    noblanks();
+
+    if (!isdigit(c=line[lp]) && c != '.' && c != '-') {
+        ++lp;
+        return c;
+    }
+
+    storenumber(val);
+
+    if (c == '.' || c == '-') {
+        if (!isdigit(line[++lp])) {
+            return c;
+        } else {
+            append(val, c);
+            storenumber(val);
+        }
+    }
+    
+    return NUMBER;
+}
+
+/* append: append a character to the end of a string */
+void append(char s[], int c)
+{
+    int l = strlen(s);
+    s[l] = c;
+    s[l+1] = '\0';
+}
+
+/* storenumber: to store number in the array */
+void storenumber(char v[])
+{
+    while (isdigit(line[lp]))  {
+        append(v, line[lp++]);
     }
 }
 
-
-
-
-/* get_line: get an entire line from input stream */
-int get_line(char line[], int lim) 
+/* get_line: get entire line from input stream, return length */
+int get_line(char line[], int lim)
 {
-    int i = 0, c;
+    int c, i;
 
-    while (--lim && (c = getchar()) != EOF && c != '\n')
-        line[i++] = c;
+    for (i = 0; i<lim-1 && (c=getchar())!=EOF && c!='\n'; ++i)
+        line[i] = c;
     if (c == '\n')
         line[i++] = c;
     line[i] = '\0';
