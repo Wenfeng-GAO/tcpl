f11a95ecbfda7b764acde6e9e161f120bec98a73
wenfeng <elricfeng@gmail.com>
2015-06-22 22:55:56 +0200

    chapter 5

--- chapter5/prog5.11.c
+++ chapter5/prog5.11.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void swap(void *v[], int i, int j);
+int numcmp(char *s1, char *s2);
 
 int main(int argc, char *argv[])
 {
-    int t[] = {1, 2, 3};
-    int *v[3];
-    for (int i = 0; i < 3; ++i)
-        *(v+i) = &t[i];
-    swap((void *)v, 0, 2);
-    for (int i = 0; i < 3; ++i)
-        printf("%d ", **(v+i));
-    printf("\n");
+    char c[] = "2";
+    char *f = "12";
+    
+    printf("%d\n", numcmp(c, f));
+
     return 0;
 
 }
@@ -24,4 +23,19 @@ void swap(void *v[], int i, int j)
     tmp = v[i];
     v[i] = v[j];
     v[j] = tmp;
+}
+
+/* numcmp: compare s1 and s2 numerically */
+int numcmp(char *s1, char *s2)
+{
+    double v1, v2;
+
+    v1 = atof(s1);
+    v2 = atof(s2);
+    if (v1 < v2)
+        return -1;
+    else if (v1 > v2)
+        return 1;
+    else
+        return 0;
 }
